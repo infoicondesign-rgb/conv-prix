6e6b764b8d477d53b92f3bfbda2c8e7f62d7c94c
infoicondesign-rgb <info.icondesign@gmail.com>
2025-08-13 09:38:55 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,11 @@
-const CACHE = "convprix-v4";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js", "./logo-192.png", "./logo-512.png", "./logo.png"];
-
+const CACHE = "convprix-v7";
+const ASSETS = ["./","./index.html","./manifest.webmanifest","./sw.js","./logo-192.png","./logo-512.png","./logo.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
 });
-
 self.addEventListener("activate", (e) => {
   e.waitUntil(self.clients.claim());
 });
-
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((r) => r || fetch(e.request).then((resp) => {
